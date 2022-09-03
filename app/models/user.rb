1f8480ab3de86fd8423018dda124267863937a41
Matthieu Seaux-Peters <matthieu.sp@gmail.com>
2022-09-03 14:03:28 +0200

    front UI navbar profile avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :flats
   has_many :bookings
+  has_one_attached :photo
 
-  validates :first_name, :last_name, :telephone, :email, presence: true
+  validates :first_name, :last_name, :telephone, :email, :photo, presence: true
   validates :email, uniqueness: true
 
   after_create :confirm_user
