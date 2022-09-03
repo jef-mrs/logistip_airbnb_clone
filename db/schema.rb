6cb0023a8abc9677647c3e9a6cbdfcb7eedad609
Matthieu Seaux-Peters <matthieu.sp@gmail.com>
2022-09-03 14:47:50 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -84,9 +84,9 @@ ActiveRecord::Schema.define(version: 2022_09_03_123923) do
     t.string "first_name"
     t.string "last_name"
     t.string "telephone"
-    t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
