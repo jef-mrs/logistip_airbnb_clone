e32e7b44146af06712f08f51ec0e0e33142b729e
STEPHANE DEBUICHE <debuiche.stephane@gmail.com>
2022-09-03 13:24:36 +0200

    homepage améliorée

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,22 @@
+// // import { Controller } from "@hotwired/stimulus"
+// import Carousel from 'stimulus-carousel'
+
+// export default class extends Carousel {
+//   connect() {
+//     super.connect()
+//     console.log('Hello from carousel controller')
+
+//     // The swiper instance.
+//     this.swiper
+
+//     // Default options for every carousels.
+//     this.defaultOptions
+//   }
+
+//   // You can set default options in this getter.
+//   get defaultOptions() {
+//     return {
+//       // Your default options here
+//     }
+//   }
+// }
